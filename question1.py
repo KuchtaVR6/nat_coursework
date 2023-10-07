23c68c583bcf567f90a4ca2dd2c13e91abff3fe8
Patryk Kuchta <kuchtavr6@gmail.com>
2023-10-07 12:58:01 +0100

    making the PSO algorithm fully

--- question1.py
+++ question1.py
@@ -1,58 +1,89 @@
 import math
+import random
+
 import numpy as np
+
+# This is the Particle Swarm Optimization
 
 ''' Setting up the problem '''
 
-problem_dimensions = 5
+problem_dimensions = 2
+min_generated_pos = 0
+max_generated_pos = 3
 
 
 def evaluate_the_fitness(given_solution):
-    given_solution[given_solution > 9] *= -2
+    copy_of_given_solution = np.copy(given_solution)
 
-    return np.sum(given_solution)
+    # entries above 9, assign a penalty to them
+    copy_of_given_solution[copy_of_given_solution > 9] *= -2
+
+    return np.sum(copy_of_given_solution)
 
 
 ''' Setting up the algorithm '''
 
-residual_velocity = 0.7
-personal_best_gravity = 2
-global_best_gravity = 2
+velocity_inertia = 0.7
+personal_best_gravity = 3
+global_best_gravity = 1
 
 global_best = [[0, 0, 0, 0, 0], -math.inf]
 
 population_size = 20
 
 
-# Particle Swarm Optimization
-
-
 class Particle:
-    def __init__(self):
-        self.position = np.random.rand(5) * 10
-        self.velocity = 0
-        self.personal_best = global_best  # correct?
+    def __init__(self, label='label not set'):
+        self.label = label
+        self.position = np.random.rand(problem_dimensions) * (max_generated_pos - min_generated_pos) + min_generated_pos
+        self.velocity = np.zeros(problem_dimensions)
+        # making a copy of global_best
+        self.personal_best = [[0, 0, 0, 0, 0], -math.inf]
 
-    def get_fitness(self):
-        current_fitness = evaluate_the_fitness(self.position)
+    def get_fitness_and_update_personal_best(self):
+        own_current_fitness = evaluate_the_fitness(self.position)
 
-        if self.personal_best[1] < current_fitness:
-            self.personal_best = [self.position, current_fitness]
+        if self.personal_best[1] < own_current_fitness:
+            self.personal_best = [self.position, own_current_fitness]
 
-        return current_fitness
+        return own_current_fitness
+
+    def update_velocities(self):
+        r_value = random.random()
+        self.velocity = self.velocity * velocity_inertia + \
+                        personal_best_gravity * r_value * (self.personal_best[0] - self.position) + \
+                        global_best_gravity * r_value * (global_best[0] - self.position)
+
+    def change_position(self):
+        self.position += self.velocity
 
     def __str__(self):
-        return 'I am a particle at ' + str(self.position) + \
-            '. My current velocity is ' + str(self.velocity) + '!'
+        return self.label+';'+';'.join(self.position.astype(str).tolist())
 
 
-# generate random solutions
+# generate particles
 
-particles = [Particle for _ in range(population_size)]
+particles = [Particle(chr(index + 65)) for index in range(population_size)]
 
 
-def update_global_best():
-    for particle in particles:
-        global global_best
-        current_fitness = particle.get_fitness()
-        if current_fitness > global_best[1]:
-            global_best = [particle.location, particle.get_fitness()]
+def generational_loop(number_of_generations):
+    # todo fix the line below
+    global global_best
+    for current_generation in range(number_of_generations):
+        for particle in particles:
+            current_fitness = particle.get_fitness_and_update_personal_best()
+            # using the best_ever scheme update the global best
+            if current_fitness > global_best[1]:
+                # take the copy of the position (to avoid reference issues)
+                global_best = [list(particle.position), current_fitness]
+
+        print('>>gen_'+str(current_generation)+'_best_fit|'+str(global_best[1])+'|at|'+str(global_best[0]))
+
+        for particle in particles:
+            particle.update_velocities()
+            particle.change_position()
+
+    return global_best
+
+
+print('Best solution is:', generational_loop(20))
