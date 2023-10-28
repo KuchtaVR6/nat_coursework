17405e4e5758fc8d73a8e47d8e04695ff0f9efab
Patryk Kuchta <kuchtavr6@gmail.com>
2023-10-28 18:47:21 +0100

    interim ready

--- Question3/main.py
+++ Question3/main.py
@@ -1,6 +1,7 @@
 from geneticProgrammingProblem import GeneticProgrammingProblem
 from geneticAlgorithmForGP import GeneticAlgorithmForGP
 
+
 def sum(x, y): return x + y
 
 
@@ -30,7 +31,7 @@ def generate_data(number_of_data, window_size, sequence_name='fibonacci'):
         sequence = [2]
 
         while len(sequence) < number_of_data + window_size:
-            sequence.append(sequence[-1]**2 - sequence[-1] + 1)
+            sequence.append(sequence[-1] ** 2 - sequence[-1] + 1)
 
     elif sequence_name == 'pell':
         sequence = [0, 1]
@@ -47,52 +48,109 @@ def generate_data(number_of_data, window_size, sequence_name='fibonacci'):
     data = []
 
     for index in range(window_size, len(sequence)):
-        data.append([sequence[index-window_size:index], sequence[index]])
+        data.append([sequence[index - window_size:index], sequence[index]])
 
     return data
 
 
-problem = GeneticProgrammingProblem(
+max_solution_depth = 5
+
+problem_fibonacci = GeneticProgrammingProblem(
     possible_function,
     possible_functions_labels,
     possible_constants,
-    3,
+    max_solution_depth,
+    generate_data(20, max_input_length, sequence_name='fibonacci'),
+    max_input_length)
+
+problem_pell = GeneticProgrammingProblem(
+    possible_function,
+    possible_functions_labels,
+    possible_constants,
+    max_solution_depth,
     generate_data(20, max_input_length, sequence_name='pell'),
     max_input_length)
 
-print("population;mutation_rate;crossover_rate;average_solution_cost;success_rate")
+problem_perrin = GeneticProgrammingProblem(
+    possible_function,
+    possible_functions_labels,
+    possible_constants,
+    max_solution_depth,
+    generate_data(20, max_input_length, sequence_name='perrin'),
+    max_input_length)
 
-for population_size in range(1, 101, 10):
-    for mutation_prob_times_10 in range(1, 10):
-        mutation_prob = mutation_prob_times_10 / 10
-        for crossover_prob_times_10 in range(1, 10):
-            crossover_prob = crossover_prob_times_10 / 10
+problem_sylvester = GeneticProgrammingProblem(
+    possible_function,
+    possible_functions_labels,
+    possible_constants,
+    max_solution_depth,
+    generate_data(6, max_input_length, sequence_name='sylvester'),
+    max_input_length)
 
-            convergence_in = 0
-            trials_failed = 0
+problem_function = GeneticProgrammingProblem(
+    possible_function,
+    possible_functions_labels,
+    possible_constants,
+    max_solution_depth,
+    generate_data(40, max_input_length, sequence_name='function'),
+    max_input_length)
 
-            for trials in range(0, 100):
-                algorithm = GeneticAlgorithmForGP(problem, population_size, mutation_prob, crossover_prob)
+problem_label = ['fib', 'per', 'syl', 'fun']
+problems = [problem_fibonacci, problem_perrin, problem_sylvester]
 
-                output = algorithm.compute_until_fit()
+for index in range(len(problems)):
+    print("==================")
+    print(problem_label[index])
+    trials_failed = 0
+    convergence_in = 0
 
-                if output == -1:
-                    trials_failed += 1
-                else:
-                    convergence_in += output
+    for trail in range(100):
+        algorithm = GeneticAlgorithmForGP(problems[index], 11, 0.3, 0.7)
 
-            successes = 100-trials_failed
+        output = algorithm.compute_until_fit()
 
-            if successes == 0:
-                print(f"{population_size};{mutation_prob};{crossover_prob};{0};{successes}")
-            else:
-                print(
-                    f"{population_size};{mutation_prob};{crossover_prob};{int(convergence_in / successes)};{successes}")
+        if output == -1:
+            trials_failed += 1
+        else:
+            print("-----------")
+            print(problem_label[index])
+            print(algorithm.best_solution_ever[0])
+            print("-----------")
+            convergence_in += output
+
+    successes = 100 - trials_failed
+
+    if successes == 0:
+        print(f"{0};{successes}")
+    else:
+        print(
+            f"{int(convergence_in / successes)};{successes}")
 
 
-
-
-
-
-
-
+# print("population;mutation_rate;crossover_rate;average_solution_cost;success_rate")
+# for population_size in range(1, 101, 10):
+#     for mutation_prob_times_10 in range(1, 10):
+#         mutation_prob = mutation_prob_times_10 / 10
+#         for crossover_prob_times_10 in range(1, 10):
+#             crossover_prob = crossover_prob_times_10 / 10
+#
+#             convergence_in = 0
+#             trials_failed = 0
+#
+#             for trials in range(0, 100):
+#                 algorithm = GeneticAlgorithmForGP(problem_pell, population_size, mutation_prob, crossover_prob)
+#
+#                 output = algorithm.compute_until_fit()
+#
+#                 if output == -1:
+#                     trials_failed += 1
+#                 else:
+#                     convergence_in += output
+#
+#             successes = 100 - trials_failed
+#
+#             if successes == 0:
+#                 print(f"{population_size};{mutation_prob};{crossover_prob};{0};{successes}")
+#             else:
+#                 print(
+#                     f"{population_size};{mutation_prob};{crossover_prob};{int(convergence_in / successes)};{successes}")
